3fc8a20720ab22445635dec2b01108a75ec974f6
Евгений <your_email@example.com>
2026-04-27 22:19:48 +0300

    fix: polyfill File/Blob for Node 19 runtime

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-import "./src/index.js";
+import { Blob as NodeBlob, File as NodeFile } from "node:buffer";
 
+if (typeof globalThis.File === "undefined" && typeof NodeFile !== "undefined") {
+  globalThis.File = NodeFile;
+}
+
+if (typeof globalThis.Blob === "undefined" && typeof NodeBlob !== "undefined") {
+  globalThis.Blob = NodeBlob;
+}
+
+await import("./src/index.js");
